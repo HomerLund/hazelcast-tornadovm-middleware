aa870bc0d04f25ba56a28f2e1dec6cbbd8e19d70
Maksim <maxim2019mail.ua@gmail.com>
2026-05-17 21:18:18 +0300

    feat: implement Layers interface

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/model/layers/Layer.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/model/layers/Layer.java
@@ -3,4 +3,7 @@ package kpi.diploma.userprojects.facerecognition.model.layers;
 import java.io.Serializable;
 
 public interface Layer extends Serializable {
+    float[] forward(float[] input);
+    float[] backward(float[] outputGradient);
+    void updateWeights(float learningRate);
 }
