d20280dd86632f246d629db4ed49b7e3ef644e64
Maksim <maxim2019mail.ua@gmail.com>
2026-05-17 22:47:40 +0300

    feat: implement SigmoidLayer class

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/model/layers/SigmoidLayer.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/model/layers/SigmoidLayer.java
@@ -1,4 +1,33 @@
 package kpi.diploma.userprojects.facerecognition.model.layers;
 
+import kpi.diploma.userprojects.facerecognition.model.math.Activations;
+
 public class SigmoidLayer implements Layer{
+    private int currentLength = 0;
+    private float[] outputCache;
+    private float[] inputGradient;
+
+    private void ensureCapacity(int length){
+        if (currentLength != length){
+            currentLength = length;
+            outputCache = new float[length];
+            inputGradient = new float[length];
+        }
+    }
+
+    @Override
+    public float[] forward(float[] input){
+        ensureCapacity(input.length);
+        Activations.sigmoidForward(input, outputCache, input.length);
+        return outputCache;
+    }
+
+    @Override
+    public float[] backward(float[] outputGradient){
+        Activations.sigmoidBackward(outputCache, outputGradient, inputGradient, currentLength);
+        return inputGradient;
+    }
+
+    @Override
+    public void updateWeights(float learningRate){}
 }
