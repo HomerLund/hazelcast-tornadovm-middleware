e1946d3dd980a5728de2be8e69e481931fc4855d
Maksim <maxim2019mail.ua@gmail.com>
2026-05-17 22:51:23 +0300

    refactor: update RunSequentialTrainer class

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/local/RunSequentialTrainer.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/local/RunSequentialTrainer.java
@@ -1,12 +1,21 @@
 package kpi.diploma.userprojects.facerecognition.local;
 
 import kpi.diploma.userprojects.facerecognition.model.core.NeuralNetwork;
+import kpi.diploma.userprojects.facerecognition.model.layers.DenseLayer;
+import kpi.diploma.userprojects.facerecognition.model.layers.ReLULayer;
+import kpi.diploma.userprojects.facerecognition.model.layers.SigmoidLayer;
+import kpi.diploma.userprojects.facerecognition.model.loss.BinaryCrossEntropy;
 
 import java.nio.file.Paths;
 
 public class RunSequentialTrainer {
     public static void main(String[] args){
-        NeuralNetwork network = new NeuralNetwork();
+        NeuralNetwork network = new NeuralNetwork(new BinaryCrossEntropy());
+
+        network.addLayer(new DenseLayer(196608, 128));
+        network.addLayer(new ReLULayer());
+        network.addLayer(new DenseLayer(128, 1));
+        network.addLayer(new SigmoidLayer());
 
         String datasetPath = Paths.get("userprojects", "facerecognition", "assets",
                 "dataset", "data", "prepared").toString();
