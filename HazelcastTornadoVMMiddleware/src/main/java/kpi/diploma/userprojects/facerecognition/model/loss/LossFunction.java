870658a4e7ef9b92d85c551e671f384e130b9175
Maksim <maxim2019mail.ua@gmail.com>
2026-05-17 22:46:10 +0300

    feat: implement LossFunction interface

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/model/loss/LossFunction.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/model/loss/LossFunction.java
@@ -3,4 +3,5 @@ package kpi.diploma.userprojects.facerecognition.model.loss;
 import java.io.Serializable;
 
 public interface LossFunction extends Serializable {
+    float[] calculateDerivative(float[] prediction, float[] target);
 }
