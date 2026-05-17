1b52996f1032502ad73f47fe197d2e5ca8283453
Maksim <maxim2019mail.ua@gmail.com>
2026-05-17 22:46:57 +0300

    feat: implement BinaryCrossEntropy class

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/model/loss/BinaryCrossEntropy.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/model/loss/BinaryCrossEntropy.java
@@ -1,4 +1,19 @@
 package kpi.diploma.userprojects.facerecognition.model.loss;
 
+import kpi.diploma.userprojects.facerecognition.model.math.LossMath;
+
 public class BinaryCrossEntropy implements LossFunction{
+    private int currentLength = 0;
+    private float[] gradientBuffer;
+
+    @Override
+    public float[] calculateDerivative(float[] prediction, float[] target){
+        if (currentLength != prediction.length){
+            currentLength = prediction.length;
+            gradientBuffer = new float[prediction.length];
+        }
+
+        LossMath.bceDerivative(prediction, target, gradientBuffer, currentLength);
+        return gradientBuffer;
+    }
 }
