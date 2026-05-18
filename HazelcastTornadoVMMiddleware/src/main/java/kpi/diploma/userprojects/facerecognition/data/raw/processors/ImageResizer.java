cb0f97994cf1e392793270fd017311d6edd9d512
Maksim <maxim2019mail.ua@gmail.com>
2026-05-18 15:27:58 +0300

    refactor: adapt ImageResizer class to new data/utils/ImageProcessingUtils class

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/data/raw/processors/ImageResizer.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/data/raw/processors/ImageResizer.java
@@ -1,6 +1,7 @@
 package kpi.diploma.userprojects.facerecognition.data.raw.processors;
 
 import kpi.diploma.userprojects.facerecognition.data.raw.RawData;
+import kpi.diploma.userprojects.facerecognition.data.utils.ImageProcessingUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -21,53 +22,12 @@ public class ImageResizer implements DataProcessor{
     @Override
     public RawData process(RawData data){
         try{
-            byte[] resizedBytes = resizeImage(data.content(), data.name());
+            byte[] resizedBytes = ImageProcessingUtils.resizeImage(data.content(), data.name(), targetWidth, targetHeight);
             return new RawData(resizedBytes, data.name());
         }
         catch (IOException e){
             throw new RuntimeException("Error: Failed to resize image: " + data.name(), e);
         }
 
-    }
-
-    private byte[] resizeImage(byte[] originalBytes, String fileName) throws IOException {
-        ByteArrayInputStream bais = new ByteArrayInputStream(originalBytes);
-        BufferedImage originalImage = ImageIO.read(bais);
-
-        if (originalImage == null){
-            throw new IOException("Error: Can't read image format");
-        }
-
-        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
-
-        Graphics2D g2d = resizedImage.createGraphics();
-
-        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        g2d.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
-
-        g2d.dispose();
-
-        String format = getExtension(fileName);
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-        if (!ImageIO.write(resizedImage, format, baos)){
-            throw new IOException("Error: Can't write image");
-        }
-
-        return baos.toByteArray();
-    }
-
-    private String getExtension(String fileName){
-        int dotIndex = fileName.lastIndexOf('.');
-
-        if (dotIndex > 0 && dotIndex < fileName.length() - 1){
-            return fileName.substring(dotIndex + 1).toLowerCase();
-        }
-
-        return "jpg";
     }
 }
