cb152e4b23dc9e1c28ad45794b6786cbb407281e
Maksim <maxim2019mail.ua@gmail.com>
2026-05-18 21:47:26 +0300

    feat: implement ImageDatasetPartitioner class

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/data/preparation/share/ImageDatasetPartitioner.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/data/preparation/share/ImageDatasetPartitioner.java
@@ -1,5 +1,45 @@
 package kpi.diploma.userprojects.facerecognition.data.preparation.share;
 
-public class DatasetSplitter {
+import kpi.diploma.middleware.core.data.distribution.DataPartitioner;
+import kpi.diploma.userprojects.facerecognition.data.runtime.readers.DatasetItem;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ImageDatasetPartitioner implements DataPartitioner<DatasetItem> {
+    @Override
+    public List<List<DatasetItem>> partition(List<DatasetItem> items, double[] proportions){
+        int numNodes = proportions.length;
+        List<List<DatasetItem>> nodeChunks = new ArrayList<>();
+        for (int i = 0; i < numNodes; i++) {
+            nodeChunks.add(new ArrayList<>());
+        }
+
+        Map<String, List<DatasetItem>> itemsByLabel = items.stream()
+                .collect(Collectors.groupingBy(DatasetItem::label));
+
+        for (Map.Entry<String, List<DatasetItem>> entry : itemsByLabel.entrySet()){
+            List<DatasetItem> labelGroup = entry.getValue();
+
+            int totalInGroup = labelGroup.size();
+            int currentStartIndex = 0;
+
+            for (int i = 0; i < numNodes; i++) {
+                int chunkSize;
+                if (i == numNodes - 1){
+                    chunkSize = totalInGroup - currentStartIndex;
+                }
+                else{
+                    chunkSize = (int) Math.round(totalInGroup * proportions[i]);
+                }
+
+                List<DatasetItem> slice = labelGroup.subList(currentStartIndex, currentStartIndex + chunkSize);
+                nodeChunks.get(i).addAll(slice);
+                currentStartIndex += chunkSize;
+            }
+        }
+        return nodeChunks;
+    }
 }
