a240871a4700f51629859801daedaa50d9445c5e
Maksim <maxim2019mail.ua@gmail.com>
2026-05-19 12:31:28 +0300

    feat: implement ClusterDataDistributor interface

---
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/client/orchestration/ClusterDataDistributor.java
@@ -0,0 +1,17 @@
+package kpi.diploma.middleware.client.orchestration;
+
+import kpi.diploma.middleware.core.data.distribution.DataPartitioner;
+import kpi.diploma.middleware.core.data.io.RemoteSourceLoader;
+import kpi.diploma.middleware.core.data.io.RemoteTargetWriter;
+
+import java.util.List;
+
+public interface ClusterDataDistributor<T> {
+    void distributeData(
+        List<T> allItems,
+        DataPartitioner<T> partitioner,
+        RemoteSourceLoader<T> sourceLoader,
+        RemoteTargetWriter<T> targetWriter,
+        double[] customProportions
+    );
+}
