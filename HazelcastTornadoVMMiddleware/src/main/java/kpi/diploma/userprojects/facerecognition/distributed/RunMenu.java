7c38222eb92094a62da3b8f74894c4f9fa5da5b1
Maksim <maxim2019mail.ua@gmail.com>
2026-05-22 17:33:44 +0300

    feat: implement RunMenu class

---
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/distributed/RunMenu.java
@@ -0,0 +1,52 @@
+package kpi.diploma.userprojects.facerecognition.distributed;
+
+import kpi.diploma.middleware.client.api.context.ClusterContext;
+import kpi.diploma.middleware.client.hazelcast.HazelcastClusterProvider;
+import kpi.diploma.middleware.client.orchestration.distribution.DistributionJob;
+import kpi.diploma.middleware.view.menu.builders.ResearchConsoleBuilder;
+import kpi.diploma.userprojects.facerecognition.data.preparation.share.ImageDatasetPartitioner;
+import kpi.diploma.userprojects.facerecognition.data.preparation.share.io.DiskImageSourceLoader;
+import kpi.diploma.userprojects.facerecognition.data.preparation.share.io.DiskTargetWriter;
+import kpi.diploma.userprojects.facerecognition.data.runtime.readers.DatasetItem;
+import kpi.diploma.userprojects.facerecognition.data.runtime.readers.DatasetSplitReader;
+
+import java.nio.file.Paths;
+import java.util.List;
+
+public class RunMenu {
+    public static void main(String[] args){
+        String hazelcastConfigFilePath = Paths.get("userprojects", "facerecognition", "assets", "config", "hazelcast.properties").toString();
+
+
+        try (ClusterContext context = ClusterContext.create(new HazelcastClusterProvider(), hazelcastConfigFilePath)){
+            String datasetPath = Paths.get("userprojects", "facerecognition", "assets",
+                    "dataset", "data", "prepared").toString();
+            List<String> extensions = List.of(".jpg", ".jpeg", ".png", ".bmp");
+            DatasetSplitReader reader = new DatasetSplitReader(datasetPath, extensions);
+            List<DatasetItem> allItems = reader.readDataset();
+
+            String sourceSubPath = Paths.get("data", "prepared").toString();
+
+            DistributionJob<DatasetItem> distributionJob =
+                    new DistributionJob.Builder<DatasetItem>()
+                            .items(allItems)
+                            .partitioner(new ImageDatasetPartitioner())
+                            .loader(new DiskImageSourceLoader())
+                            .writer(new DiskTargetWriter(sourceSubPath, "workspace"))
+                            .build();
+
+
+            ResearchConsoleBuilder.create("Face Recognition Research")
+                    .addStandardTask("Distribute Face Dataset to Workers", () -> {
+                        context.<DatasetItem>getDataDistributor().distributeData(distributionJob);
+                    })
+                    .addStandardTask("Set Shut Down Signal to Cluser",  () -> {
+                        context.getSystemManager().shutdownAllWorkersNode();
+                    })
+                    .start();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
