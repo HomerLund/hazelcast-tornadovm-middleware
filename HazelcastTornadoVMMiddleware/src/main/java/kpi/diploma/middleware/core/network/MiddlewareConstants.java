93ec596dc8c6757cf66731bfb8674f5f6dcd727f
Maksim <maxim2019mail.ua@gmail.com>
2026-05-23 20:28:44 +0300

    refactor: update MiddlewareConstants

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/core/network/MiddlewareConstants.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/core/network/MiddlewareConstants.java
@@ -1,5 +1,8 @@
 package kpi.diploma.middleware.core.network;
 
 public final class MiddlewareConstants {
+    private MiddlewareConstants(){}
+
     public static final String SYSTEM_POOL_NAME = "system-cluster-pool";
+    public static final String SYSTEM_SANDBOX_FOLDER_NAME = "nodes";
 }
