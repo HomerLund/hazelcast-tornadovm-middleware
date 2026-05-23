103740d4482a719da36c541036c0110bc310caa6
Maksim <maxim2019mail.ua@gmail.com>
2026-05-23 14:52:05 +0300

    feat: update DatasetItem

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/data/runtime/readers/DatasetItem.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/userprojects/facerecognition/data/runtime/readers/DatasetItem.java
@@ -1,9 +1,16 @@
 package kpi.diploma.userprojects.facerecognition.data.runtime.readers;
 
 import java.io.Serializable;
+import java.nio.file.Paths;
 
 public record DatasetItem(
         String filePath,
         String label,
         boolean isTrain
-) implements Serializable {}
+) implements Serializable {
+    public String getRelativeSavePath(){
+        String fileName = Paths.get(filePath).getFileName().toString();
+        String splitFolder = isTrain ? "train" : "test";
+        return  Paths.get(splitFolder, fileName).toString();
+    }
+}
