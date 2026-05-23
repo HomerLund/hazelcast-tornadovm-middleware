bb6fa2eaae3616ad6024e04d1d400376d6916af6
Maksim <maxim2019mail.ua@gmail.com>
2026-05-23 20:29:26 +0300

    refactor: update RemoteTargetWriter

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/core/data/io/RemoteTargetWriter.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/core/data/io/RemoteTargetWriter.java
@@ -3,5 +3,5 @@ package kpi.diploma.middleware.core.data.io;
 import java.io.Serializable;
 
 public interface RemoteTargetWriter<T> extends Serializable {
-    void writeToDisk(T metadata, byte[] content, String workerNodeId);
+    void writeToDisk(T metadata, byte[] content, String workspaceNodePath);
 }
