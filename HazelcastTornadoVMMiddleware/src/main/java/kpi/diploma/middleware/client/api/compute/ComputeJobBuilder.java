92f4043870b231ba67b11f744e0e65f479ee039f
Maksim <maxim2019mail.ua@gmail.com>
2026-05-25 18:54:05 +0300

    feat: update ComputeJobBuilder

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/client/api/compute/ComputeJobBuilder.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/client/api/compute/ComputeJobBuilder.java
@@ -2,9 +2,12 @@ package kpi.diploma.middleware.client.api.compute;
 
 import kpi.diploma.middleware.client.orchestration.compute.ComputeJob;
 import kpi.diploma.middleware.core.function.SerializableFunction;
-import kpi.diploma.middleware.core.network.tasks.compute.RemoteDiskScanTask;
+import kpi.diploma.middleware.core.network.tasks.compute.RemoteDiskCacheSetupTask;
+import kpi.diploma.middleware.core.network.tasks.compute.RemoteRamCacheSetupTask;
 
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
 
 public class ComputeJobBuilder<I, O> {
     public enum SourceStrategy{
@@ -17,6 +20,7 @@ public class ComputeJobBuilder<I, O> {
     private String baseDiskDirectory;
 
     private List<I> clientRamData;
+    private Function<Integer, List<O>> clientPartitioner;
 
     private String targetPoolName;
     private SerializableFunction<I, O> userLambda;
@@ -34,9 +38,9 @@ public class ComputeJobBuilder<I, O> {
         return this;
     }
 
-    public ComputeJobBuilder<I, O> sourceFromClientRam(List<I> clientRamData){
+    public ComputeJobBuilder<I, O> sourceFromClientRam(Function<Integer, List<O>> clientPartitioner){
         this.sourceStrategy = SourceStrategy.CLIENT_RAM;
-        this.clientRamData = clientRamData;
+        this.clientPartitioner = clientPartitioner;
         return this;
     }
 
@@ -56,7 +60,7 @@ public class ComputeJobBuilder<I, O> {
     }
 
     public ComputeJob<Void> buildSetupJob(){
-        if (targetPoolName == null || userLambda == null || cashKey == null){
+        if (targetPoolName == null || (userLambda == null && clientPartitioner == null) || cashKey == null){
             throw new IllegalStateException("Incomplete task setup");
         }
 
@@ -64,10 +68,29 @@ public class ComputeJobBuilder<I, O> {
             case WORKER_DISK:
                 @SuppressWarnings("unchecked")
                 SerializableFunction<String, O> diskLambda = (SerializableFunction<String, O>) userLambda;
-                RemoteDiskScanTask<O> diskTask = new RemoteDiskScanTask<>(baseDiskDirectory, diskLambda, cashKey);
+                RemoteDiskCacheSetupTask<O> diskTask = new RemoteDiskCacheSetupTask<>(baseDiskDirectory, diskLambda, cashKey);
                 return new ComputeJob.Builder<Void>()
                         .poolName(targetPoolName)
                         .task(diskTask)
+                        .build();
+            case CLIENT_RAM:
+                Function<Integer, List<Callable<Void>>> taskGenerator = (nodeCount) -> {
+                    List<O> userPartitions = clientPartitioner.apply(nodeCount);
+
+                    if (userPartitions.size() != nodeCount){
+                        throw new IllegalStateException("The user's lambda returned " + userPartitions.size() + "items, but the expected result was " + nodeCount);
+                    }
+
+                    List<Callable<Void>> tasks = new java.util.ArrayList<>();
+                    for (O partition : userPartitions){
+                        tasks.add(new RemoteRamCacheSetupTask<>(partition, cashKey));
+                    }
+
+                    return tasks;
+                };
+                return new ComputeJob.Builder<Void>()
+                        .poolName(targetPoolName)
+                        .targetedGenerator(taskGenerator)
                         .build();
             default:
                 return new ComputeJob.Builder<Void>()
