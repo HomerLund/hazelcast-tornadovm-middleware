48f96c236490219a09ececdfd68c320a47fc39f6
Maksim <maxim2019mail.ua@gmail.com>
2026-05-25 16:08:16 +0300

    feat: rename RemoteSetupTask to RemoteDiskScanTask and update RemoteDiskScanTask

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/core/network/tasks/compute/RemoteDiskScanTask.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/core/network/tasks/compute/RemoteDiskScanTask.java
@@ -1,32 +1,54 @@
 package kpi.diploma.middleware.core.network.tasks.compute;
 
+import com.hazelcast.cluster.Member;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.HazelcastInstanceAware;
 import kpi.diploma.middleware.core.context.NodeLocalWorkspace;
 import kpi.diploma.middleware.core.function.SerializableFunction;
 import kpi.diploma.middleware.core.logging.Logger;
+import kpi.diploma.middleware.core.network.MiddlewareConstants;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.function.Function;
 
-public class RemoteSetupTask<O> implements Callable<Void>, Serializable {
+public class RemoteDiskScanTask<O> implements Callable<Void>, Serializable, HazelcastInstanceAware {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private final SerializableFunction<Void, O> userLambda;
+    private final String baseDirectory;
+    private final SerializableFunction<String, O> userLambda;
     private final String cashKey;
 
-    public RemoteSetupTask(SerializableFunction<Void, O> userLambda, String cashKey) {
+    private transient HazelcastInstance hazelcastInstance;
+
+    public RemoteDiskScanTask(String baseDirectory, SerializableFunction<String, O> userLambda, String cashKey) {
+        this.baseDirectory = baseDirectory;
         this.userLambda = userLambda;
         this.cashKey = cashKey;
     }
 
     @Override
+    public void setHazelcastInstance(HazelcastInstance hazelcastInstance){
+        this.hazelcastInstance = hazelcastInstance;
+    }
+
+    @Override
     public Void call() throws Exception {
         try {
-            O result = userLambda.apply(null);
+            Member localMember = hazelcastInstance.getCluster().getLocalMember();
+
+            String host = localMember.getAddress().getHost();
+            int port = localMember.getAddress().getPort();
+
+            String nodeName = "node-" + host.replace(".", "-") + "_" + port;
+
+            String finalPath =  Paths.get(baseDirectory, MiddlewareConstants.SYSTEM_SANDBOX_FOLDER_NAME, nodeName).toString();
+
+            O result = userLambda.apply(finalPath);
 
             if (cashKey != null && result != null) {
                 if (result instanceof Collection<?> collection) {
