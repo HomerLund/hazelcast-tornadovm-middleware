2d2ec93a3db7190b14cfc4c5f3f46cae2fa24a50
Maksim <maxim2019mail.ua@gmail.com>
2026-05-28 22:14:35 +0300

    feat: update RemoteTransformTask

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/core/network/tasks/compute/pipeline/RemoteTransformTask.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/core/network/tasks/compute/pipeline/RemoteTransformTask.java
@@ -2,6 +2,7 @@ package kpi.diploma.middleware.core.network.tasks.compute.pipeline;
 
 import kpi.diploma.middleware.core.context.NodeLocalWorkspace;
 import kpi.diploma.middleware.core.function.SerializableFunction;
+import kpi.diploma.middleware.core.logging.Logger;
 import kpi.diploma.middleware.core.network.MiddlewareConstants;
 
 import java.io.Serial;
@@ -18,23 +19,28 @@ public class RemoteTransformTask<I, O> implements Callable<Void>, Serializable {
     private final String inputKey;
     private final String outputKey;
     private final SerializableFunction<I, O> lambda;
+    private final int parallelism;
 
-    public RemoteTransformTask(String inputKey, String outputKey, SerializableFunction lambda) {
+    public RemoteTransformTask(String inputKey, String outputKey, SerializableFunction lambda, int parallelism) {
         this.inputKey = inputKey;
         this.outputKey = outputKey;
         this.lambda = lambda;
+        this.parallelism = parallelism;
     }
 
     @Override
     public Void call() throws Exception {
         try {
+            Logger.info("RemoteTransformTask", "Starting");
 
-
-            Queue<I> inQueue = NodeLocalWorkspace.getOrCreateQueue(inputKey);
+            Queue<I> inQueue = NodeLocalWorkspace.waitForQueue(inputKey);
             BlockingQueue<O> outQueue = NodeLocalWorkspace.getOrCreateBlockingQueue(outputKey, MiddlewareConstants.MAX_CHANNEL_CAPACITY);
+
+            NodeLocalWorkspace.registerProducers(outputKey, parallelism);
 
             I item;
 
+            int processed = 0;
             while (true) {
                 if (inQueue instanceof BlockingQueue) {
                     item = ((BlockingQueue<I>) inQueue).poll(MiddlewareConstants.MAX_CHANNEL_CAPACITY, TimeUnit.MILLISECONDS);
@@ -43,7 +49,7 @@ public class RemoteTransformTask<I, O> implements Callable<Void>, Serializable {
                 }
 
                 if (item == null) {
-                    if (NodeLocalWorkspace.isEndOfStream() && inQueue.isEmpty()) {
+                    if (NodeLocalWorkspace.inQueueFinished(inputKey) && inQueue.isEmpty()) {
                         break;
                     }
 
@@ -52,14 +58,21 @@ public class RemoteTransformTask<I, O> implements Callable<Void>, Serializable {
 
                 O result = lambda.apply(item);
                 outQueue.put(result);
+
+                processed++;
+                Logger.info("RemoteTransformTask", "processed " + processed + "/" + inQueue.size());
             }
 
+            Logger.info("RemoteTransformTask", "Ending");
             return null;
         }
         catch (Exception e) {
             System.err.println("Error in RemoteTransformTask: ");
             e.printStackTrace();
             throw e;
+        }
+        finally {
+            NodeLocalWorkspace.notifyProducerFinished(outputKey);
         }
     }
 
