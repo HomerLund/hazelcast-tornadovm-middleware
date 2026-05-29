ab19657272ba2c3b332aa49cd36006d2a1dca61e
Maksim <maxim2019mail.ua@gmail.com>
2026-05-29 23:25:54 +0300

    feat: update MiddlewareConstants

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/core/network/MiddlewareConstants.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/core/network/MiddlewareConstants.java
@@ -5,6 +5,7 @@ public final class MiddlewareConstants {
 
     public static final String SYSTEM_POOL_NAME = "system-cluster-pool";
     public static final String SYSTEM_SANDBOX_FOLDER_NAME = "nodes";
+    public static final String SYSTEM_ACCELERATOR_CACHE_KEY = "global_node_gpu_context";
     public static final int MAX_CHANNEL_CAPACITY = 100;
     public static final int MAX_BATCH_QUEUE_CAPACITY = 3;
 }
