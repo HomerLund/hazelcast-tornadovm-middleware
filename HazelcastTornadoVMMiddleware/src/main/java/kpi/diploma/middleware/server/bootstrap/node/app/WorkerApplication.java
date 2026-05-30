3682236abb8655cb909be58cc319493854e8643f
Maksim <maxim2019mail.ua@gmail.com>
2026-05-30 13:37:40 +0300

    feat: update WorkerApplication

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/server/bootstrap/node/app/WorkerApplication.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/server/bootstrap/node/app/WorkerApplication.java
@@ -1,29 +1,42 @@
 package kpi.diploma.middleware.server.bootstrap.node.app;
 
+import kpi.diploma.middleware.client.api.gpu.GpuKernel;
+import kpi.diploma.middleware.client.api.gpu.GpuParallel;
 import kpi.diploma.middleware.core.context.NodeLocalWorkspace;
 import kpi.diploma.middleware.core.logging.Logger;
 import kpi.diploma.middleware.core.network.MiddlewareConstants;
 import kpi.diploma.middleware.server.adapters.accelerator.execution.ServerGpuContext;
 import kpi.diploma.middleware.server.adapters.accelerator.tornadovm.TornadoVMAccelerator;
+import kpi.diploma.middleware.server.adapters.accelerator.tornadovm.bytebuddy.TornadoVMAnnotationRemapper;
 import kpi.diploma.middleware.server.bootstrap.accelerator.HardwareAccelerator;
+import kpi.diploma.middleware.server.bootstrap.accelerator.instrumentation.ByteBuddyAgentInstaller;
+import kpi.diploma.middleware.server.bootstrap.accelerator.instrumentation.GpuKernelInterceptor;
 import kpi.diploma.middleware.server.bootstrap.node.ClusterWorkerNode;
 import kpi.diploma.middleware.server.bootstrap.node.config.PropertyFileReader;
 import kpi.diploma.middleware.server.bootstrap.node.factory.WorkerNodeFactory;
 import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.asm.AsmVisitorWrapper;
+import net.bytebuddy.description.field.FieldDescription;
+import net.bytebuddy.description.field.FieldList;
+import net.bytebuddy.description.method.MethodList;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.implementation.Implementation;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.jar.asm.*;
+import net.bytebuddy.matcher.ElementMatcher;
+import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.pool.TypePool;
 
+import javax.swing.text.Element;
 import java.util.Properties;
 
 public class WorkerApplication {
     public static void run(String propertiesFilePath){
         System.out.println("Starting worker...");
 
-        try{
-            ByteBuddyAgent.install();
-            System.out.println("ByteBuddy Agent successfully installed");
-        }
-        catch (Exception e){
-            System.err.println("Failed to install ByteBuddy Agent: " + e.getMessage());
-        }
+        AsmVisitorWrapper.ForDeclaredMethods.MethodVisitorWrapper annotationRemapper = new TornadoVMAnnotationRemapper();
+        ByteBuddyAgentInstaller.install(annotationRemapper);
 
         try{
             HardwareAccelerator accelerator = new TornadoVMAccelerator();
