b933ed2dd82c571ad06164456a9a7e0304b8a9af
Maksim <maxim2019mail.ua@gmail.com>
2026-05-31 17:28:02 +0300

    fix: update GpuParallel annotation

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/client/api/gpu/GpuParallel.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/client/api/gpu/GpuParallel.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.CLASS)
-@Target(ElementType.LOCAL_VARIABLE)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
 public @interface GpuParallel {
 }
