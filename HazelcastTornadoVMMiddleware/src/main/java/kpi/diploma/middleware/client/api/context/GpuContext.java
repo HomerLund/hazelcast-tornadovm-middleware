a9b3b415f5fad1f8d056ab33a9885868516abe3f
Maksim <maxim2019mail.ua@gmail.com>
2026-06-01 16:02:10 +0300

    feat: update GpuContext

--- HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/client/api/context/GpuContext.java
+++ HazelcastTornadoVMMiddleware/src/main/java/kpi/diploma/middleware/client/api/context/GpuContext.java
@@ -1,7 +1,10 @@
 package kpi.diploma.middleware.client.api.context;
 
+import kpi.diploma.middleware.server.adapters.accelerator.execution.GpuMemoryExtractor;
+
 import java.util.function.Supplier;
 
 public interface GpuContext {
     <T> T executeOnGpu(String cacheKey, Object memoryContext, Supplier<T> gpuLogic);
+    void syncToHost(String cacheKey);
 }
